868f151d60ec000765cd3bbe8f5383b0cb44e479
Bene Máté <mate.bene0502@gmail.com>
2022-07-17 17:45:10 +0200

    Webcam feed added

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import left, right
 import cv2 as cv
 import numpy as np
 import cvzone as cvz
@@ -10,6 +9,29 @@ def rescaleFrame(frame, scale=0.75):
 
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
+cam = cv.VideoCapture(0)
+
+while True:
+    check, board = cam.read()
+
+    mask_red = cv.inRange(board, (0,0,50), (150,100,255))
+    mask_post_it = cv.inRange(board, (120,60,50), (200,150,100))
+    board_contours, contourFound = cvz.findContours(board, mask_post_it, 1000)
+
+    left_center = contourFound[0]['center']
+    right_center = contourFound[3]['center']
+    top_center = contourFound[1]['center'] if contourFound[1]['center'][1] < contourFound[2]['center'][1] else contourFound[2]['center']
+    bottom_center = contourFound[1]['center'] if contourFound[1]['center'][1] > contourFound[2]['center'][1] else contourFound[2]['center']
+    cv.line(board, left_center, right_center, [0, 255, 0], 2)
+    cv.line(board, top_center, bottom_center, [0, 255, 0], 2)
+
+    cv.imshow('video', board)
+
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
 
 board = cv.imread('Images/board.jpg')
 board = rescaleFrame(board, 0.25)
